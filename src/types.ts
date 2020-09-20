4941d34d1094d1b7c18a9805f4c514d800b20169
mikeyim337 <mk337337@gmail.com>
2020-09-20 01:17:16 -0700

    about to install redis

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,8 @@
-export type MyContext {
-    em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>
-}
+import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
+import { Request, Response, Express } from "express";
+
+export type MyContext = {
+  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
+  req: Request & { session: Express.Session };
+  res: Response;
+};
