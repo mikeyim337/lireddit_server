b603795cf6f1379330e2337f605d1f1bf45ef448
mikeyim337 <mk337337@gmail.com>
2020-09-20 17:14:28 -0700

    done with using redis for storing cookies

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -6,6 +6,7 @@ import {
   InputType,
   ObjectType,
   Field,
+  Query,
 } from "type-graphql";
 import { User } from "../entities/User";
 import { MyContext } from "../types";
@@ -38,10 +39,21 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em }: MyContext) {
+    //you are not logged in
+    if (!req.session.userId) {
+      return null;
+    }
+
+    const user = await em.findOne(User, { id: req.session.userId });
+    return user;
+  }
+
   @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     if (options.username.length <= 2) {
       return {
@@ -84,8 +96,12 @@ export class UserResolver {
           ],
         };
       }
-      console.log("message: ", err.message);
     }
+
+    //store user id session
+    //this will set a cookie on the user and keep them logged in
+    req.session.userId = user.id;
+
     return { user };
   }
 
